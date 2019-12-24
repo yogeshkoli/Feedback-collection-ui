f0ab78dd74e678d2a05fef966e36785a354fa67b
Yogesh Koli <yogesh@Yogeshs-MacBook-Pro.local>
2019-12-24 17:44:32 +0530

    Refactor functional component to class base component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
@@ -9,19 +9,22 @@ import Dashboard from './Dashboard';
 
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
-const App = () => {
-    return (
-        <div className="main">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="main">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
