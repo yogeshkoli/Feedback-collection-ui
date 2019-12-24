3222d2841961908fd48261596783b3c49ac8129b
Yogesh Koli <yogesh@Yogeshs-MacBook-Pro.local>
2019-12-24 15:01:26 +0530

    instaling and configuring redux thunk

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/App';
@@ -11,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}>
